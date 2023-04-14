2d7c4c1cada9c9ffbae8a510f1aefa2c302a1dbc
Toxijuice <toxijuice@gmail.com>
2023-04-14 04:51:53 -0700

    New logo applied

--- src/script/favicon.ts
+++ src/script/favicon.ts
@@ -9,7 +9,7 @@ class FaviconHandler {
     const link: HTMLLinkElement = document.createElement('link')
     link.rel = 'icon'
     link.type = 'image/svg+xml'
-    link.href = 'https://gw.alipayobjects.com/zos/bmw-prod/51a51720-8a30-4430-b6c9-be5712364f04.svg'
+    link.href = 'https://raw.githubusercontent.com/Toxijuice/sd-web-ui-bitchen-theme/main/assets/bitchenfavicon.svg'
     document.getElementsByTagName('head')[0].appendChild(link)
   }
 }
